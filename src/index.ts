1ec62bb31d21d4d9e665990f05f7ae93ee642ef4
yaseensdk <yassinesadik57@gmail.com>
2024-01-22 15:52:47 +0100

    Setup Express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('Hello TS')
+import express from 'express'
+import http from 'http'
+import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
+import compression from 'compression'
+import cors from 'cors'
+
+const app = express()
+
+app.use(
+  cors({
+    credentials: true,
+  })
+)
+
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(compression())
+
+const server = http.createServer(app)
+
+server.listen(8080, () => {
+  console.log('Server running on http://localhost:8080/')
+})
